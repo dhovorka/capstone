52bae2ac9f2c294433d07e11db2edc95c85e447e
Daniel Hovorka <dhovorka10@gmail.com>
2019-03-31 16:12:19 -0500

    added controllers

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,8 @@
 class Api::CommentsController < ApplicationController
+
+def index
+  @comments = Comment.all
+  render "index.json.jbuilder"
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+
+get "/comments" => "comments#index"
+
+end
 end
