cb27e134fb87882fd0781372afad657a0572c2dd
Daniel Hovorka <dhovorka10@gmail.com>
2019-05-14 19:42:40 -0500

    added additional params to tournament create and update routes

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -5,7 +5,10 @@ def create
   @tournament = Tournament.new(
   name: params[:name],
   location: params[:location],
-  description: params[:description]
+  description: params[:description],
+  image: params[:image],
+  coordinates: params[:coordinates],
+  website: params[:website]
   )
 if @tournament.save
   render "show.json.jbuilder"
@@ -26,7 +29,10 @@ def update
   @tournament = Tournament.find_by(id: params[:id])
   @tournament.name = params[:name] || @tournament.name
   @tournament.location = params[:location] || @tournament.location
-  @tournament.description = params[:description] || @tournament.description 
+  @tournament.description = params[:description] || @tournament.description
+  @tournament.image = params[:image] || @tournament.image
+  @tournament.coordinates = params[:coordinates] || @tournament.coordinates
+  @tournament.website = params[:website] || @tournament.website
   @tournament.save
   render "show.json.jbuilder"
 end
